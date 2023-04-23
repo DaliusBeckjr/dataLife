5ec4f11113c8282ec786fd6f0b389b79ef656630
Dalius Beck <daliusbeckxiv@gmail.com>
2023-04-23 10:50:17 -0700

    working on it

--- b_strap_prac/server.py
+++ b_strap_prac/server.py
@@ -1,3 +1,20 @@
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+@app.route('/play')
+def home(times = 3, color = 'yellow'):
+    return render_template('home.html', times=times, color=color)
+
+@app.route('/play/<int:times>')
+def repeat(times, color= 'yellow'):
+    return render_template('home.html', times=times, color=color)
+
+@app.route('/play/<int:times>/<color>')
+def color(times, color):
+    return render_template('home.html', times=times, color=color)
+
+if __name__ == '__main__':
+    app.run(debug = True, port= 5001)
 
 
